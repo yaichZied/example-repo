dee57e8af8ed220253031accdc9a41128634115d
yaichZied <zied.yaich5@gmail.com>
2023-02-06 21:19:56 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -121,7 +121,7 @@ resource "azurerm_linux_virtual_machine" "mtc-vm" {
 
   admin_ssh_key {
     username   = "adminuser"
-    public_key = file("/builds/ziedYaich/demo-pfa/example-repo/pfa-rsa.pub")
+    public_key = file("pfa-rsa.pub")
   }
 
   os_disk {
